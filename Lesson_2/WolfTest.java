8d25aa105a36b143b08e62405cb337acdd9ee994
AAKurnakov <AAKurnakov@ya.ru>
2025-12-22 16:44:17 +0300

    Модифицировал классы Wolf и WolfTest

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,13 +1,14 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf firstWolf = new Wolf();
-        firstWolf.sex = "Female";
-        firstWolf.name = "Julie";
-        firstWolf.weight = 40;
-        firstWolf.age = 5;
-        firstWolf.color = "Black";
+        firstWolf.setSex("Female");
+        firstWolf.setName("Julie");
+        firstWolf.setWeight(38);
+        firstWolf.setAge(5);
+        firstWolf.setColor("Black");
         System.out.printf("Пол - %s%nКличка - %s%nВес - %d%nВозраст - %d%nОкрас - %s%n", 
-                firstWolf.sex, firstWolf.name, firstWolf.weight, firstWolf.age, firstWolf.color);
+                firstWolf.getSex(), firstWolf.getName(), firstWolf.getWeight(), 
+                firstWolf.getAge(), firstWolf.getColor());
         firstWolf.walk();
         firstWolf.sit();
         firstWolf.run();
