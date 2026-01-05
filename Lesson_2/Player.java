4f0c74b8a9a8e05b0f671b85d78883dd1e3a2d99
AAKurnakov <AAKurnakov@ya.ru>
2026-01-05 18:02:34 +0300

    Забыл добавить класс Player. Поправил

---
+++ Lesson_2/Player.java
@@ -0,0 +1,21 @@
+public class Player {
+    private String name;
+    
+    public Player(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Имя игрока не может быть пустым");
+        }
+        this.name = name.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Имя игрока не может быть пустым");
+        }
+        this.name = name.trim();
+    }
+}
